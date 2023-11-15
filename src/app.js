a85d0a3f98bce05037c5d395fbae39fbf00ead53
Brian Villanueva <brian.villanueva@student.vinci.be>
2023-11-15 18:40:36 +0100

    test(app.js): added test case 2 names are provided and adapted code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 function greet (name) {
-  if (!name || name === undefined || name === ' ') return 'Hello, my friend.'
-  if (name === name.toUpperCase()) return 'HELLO, ' + name + '.'
-  return 'Hello, ' + name + '.'
-}
+  if(Array.isArray(name)){
+    return 'Hello, ' + name[0] + ' and ' + name[1] + '.';
+  }
+  if (!name  || name === ' ') return 'Hello, my friend.';
+  if (name === name.toUpperCase()) return 'HELLO, ' + name + '.';
+  return 'Hello, ' + name + '.';
+};
+
+
 module.exports = greet
--- src/app.test.js
+++ src/app.test.js
@@ -3,14 +3,19 @@ const greet = require('./app')
 
 test('greeting a user', () => {
   expect(greet('Jean-Kevin')).toBe('Hello, Jean-Kevin.')
-})
+});
 
 test('greeting null', () => {
   expect(greet()).toBe('Hello, my friend.')
   expect(greet(undefined)).toBe('Hello, my friend.')
   expect(greet(' ')).toBe('Hello, my friend.')
-})
+});
 
 test('greeting CAPSLOCK name', () => {
   expect(greet('KENOBI')).toBe('HELLO, KENOBI.')
-})
+});
+
+test('greeting 2 names', () => {
+
+  expect(greet(['Jill','Jane'])).toBe('Hello, Jill and Jane.')
+});
