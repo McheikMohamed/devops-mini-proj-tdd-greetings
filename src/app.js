55a7752ae2d81c5730a371cdf0433eda8e9288e8
Brian Villanueva <brian.villanueva@student.vinci.be>
2023-11-15 19:03:36 +0100

    refactor(app.js): refactoring code

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
-function greet (name) {
-  if(Array.isArray(name)){
-    if(name.length >2){
-      let retour ='Hello';
-      for(let i =0;i<name.length-1;i++){
-        retour += ', ' + name[i];
-      }
-      retour += ' and ' + name[name.length-1] + '.';
-      console.log(retour);
-      return retour;
+function greet(name) {
+  if (Array.isArray(name)) {
+    let retour = 'Hello';
+    for (let i = 0; i < name.length - 1; i++) {
+      retour += ', ' + name[i];
     }
-    return 'Hello, ' + name[0] + ' and ' + name[1] + '.';
+    retour += ' and ' + name[name.length - 1] + '.';
+    return retour;
   }
-  if (!name  || name === ' ') return 'Hello, my friend.';
+  if (!name || name === ' ') return 'Hello, my friend.';
   if (name === name.toUpperCase()) return 'HELLO, ' + name + '.';
   return 'Hello, ' + name + '.';
 };
