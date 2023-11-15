abe51e0705b75303e5acae906249405fa2194bed
MomzMc <mohamed.mcheik@student.vinci.be>
2023-11-15 16:18:00 +0100

    test(app.js,app.js.test): adding and fixing code for test for greeting ([Null,Undefined,' '])

--- src/app.test.js
+++ src/app.test.js
@@ -4,3 +4,9 @@ const greet = require('./app')
 test('greeting a user', () => {
   expect(greet('Jean-Kevin')).toBe('Hello, Jean-Kevin.')
 })
+
+test('greeting null', () => {
+  expect(greet()).toBe('Hello, my friend.')
+  expect(greet(undefined)).toBe('Hello, my friend.')
+  expect(greet(' ')).toBe('Hello, my friend.')
+})
