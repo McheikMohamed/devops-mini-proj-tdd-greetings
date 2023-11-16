2e46575a6e8ac55e779ce0383eec8c274710abae
Gabriel Paquet <58020@etu.he2b.be>
2023-11-16 15:13:13 +0100

    refactor (app.js): refactoring code (formatter)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function greet(name, language = 'en') {
+function greet(name, language = "en") {
     if (Array.isArray(name)) {
         //Lowercase/Uppercase names counter
         let upperNames = 0;
@@ -13,18 +13,18 @@ function greet(name, language = 'en') {
         //String that is going to end up containing other names
         let retourPart2;
 
-        switch(language){
-          case 'fr':
-            retourPart1 = 'Bonjour';
-            retourPart2 = ' ET BONJOUR';
-            break;
-          case 'nl':
-            retourPart1 = 'Hallo';
-            retourPart2 = ' EN HALLO';
-            break;
-          default:
-            retourPart1 = 'Hello';
-            retourPart2 = ' AND HELLO';
+        switch (language) {
+            case "fr":
+                retourPart1 = "Bonjour";
+                retourPart2 = " ET BONJOUR";
+                break;
+            case "nl":
+                retourPart1 = "Hallo";
+                retourPart2 = " EN HALLO";
+                break;
+            default:
+                retourPart1 = "Hello";
+                retourPart2 = " AND HELLO";
         }
 
         for (let i = 0, j = 0, k = 0; i < name.length; i++) {
@@ -34,9 +34,12 @@ function greet(name, language = 'en') {
                 else {
                     j++;
                     if (j === 1) retourPart2 += " " + name[i];
-                    else if (j === upperNames && language === 'fr') retourPart2 += " ET " + name[i] + " !";
-                    else if (j === upperNames && language === 'nl') retourPart2 += " EN " + name[i] + " !";
-                    else if (j === upperNames) retourPart2 += " AND " + name[i] + " !";
+                    else if (j === upperNames && language === "fr")
+                        retourPart2 += " ET " + name[i] + " !";
+                    else if (j === upperNames && language === "nl")
+                        retourPart2 += " EN " + name[i] + " !";
+                    else if (j === upperNames)
+                        retourPart2 += " AND " + name[i] + " !";
                     else retourPart2 += ", " + name[i];
                 }
             }
@@ -46,9 +49,12 @@ function greet(name, language = 'en') {
                 if (lowerNames === 1) retourPart1 += ", " + name[i] + ".";
                 else {
                     k++;
-                    if (k === lowerNames && language === 'fr') retourPart1 += " et " + name[i] + ".";
-                    else if (k === lowerNames && language === 'nl') retourPart1 += " en " + name[i] + ".";
-                    else if (k === lowerNames) retourPart1 += " and " + name[i] + ".";
+                    if (k === lowerNames && language === "fr")
+                        retourPart1 += " et " + name[i] + ".";
+                    else if (k === lowerNames && language === "nl")
+                        retourPart1 += " en " + name[i] + ".";
+                    else if (k === lowerNames)
+                        retourPart1 += " and " + name[i] + ".";
                     else retourPart1 += ", " + name[i];
                 }
             }
@@ -58,18 +64,18 @@ function greet(name, language = 'en') {
         if (lowerNames === 0) return retourPart2;
         return retourPart1 + retourPart2;
     }
-    if (!name || name === " "){
-      if(language === 'fr') return "Bonjour, mon ami.";
-      else if(language === 'nl') return "Hallo, mijn vriend.";
-      else return "Hello, my friend.";
-    } 
-    if (name === name.toUpperCase()) {
-      if(language === 'fr') return 'BONJOUR, ' + name + '.';
-      else if(language === 'nl') return 'HALLO, ' + name + '.';
-      else return "HELLO, " + name + ".";
+    if (!name || name === " ") {
+        if (language === "fr") return "Bonjour, mon ami.";
+        else if (language === "nl") return "Hallo, mijn vriend.";
+        else return "Hello, my friend.";
     }
-    if(language === 'fr') return 'Bonjour, ' + name + '.';
-    else if(language === 'nl') return 'Hallo, ' + name + '.';
+    if (name === name.toUpperCase()) {
+        if (language === "fr") return "BONJOUR, " + name + ".";
+        else if (language === "nl") return "HALLO, " + name + ".";
+        else return "HELLO, " + name + ".";
+    }
+    if (language === "fr") return "Bonjour, " + name + ".";
+    else if (language === "nl") return "Hallo, " + name + ".";
     return "Hello, " + name + ".";
 }
 
