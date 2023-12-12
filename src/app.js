d9cf811d52061756f3be24b4a256b09474f8f5f9
MomzMc <mohamed.mcheik@student.vinci.be>
2023-12-12 18:21:50 +0100

    Test : Add new test and updating code

--- src/app.js
+++ src/app.js
@@ -12,35 +12,50 @@ function greet (name, language = 'en') {
     let retourPart1
     // String that is going to end up containing other names
     let retourPart2
+    // List that only contains uppercase names
+    let retourPart3
 
     switch (language) {
       case 'fr':
         retourPart1 = 'Bonjour'
         retourPart2 = ' ET BONJOUR'
+        retourPart3 = retourPart1.toUpperCase()
         break
       case 'nl':
         retourPart1 = 'Hallo'
         retourPart2 = ' EN HALLO'
+        retourPart3 = retourPart1.toUpperCase()
         break
       default:
         retourPart1 = 'Hello'
         retourPart2 = ' AND HELLO'
+        retourPart3 = retourPart1.toUpperCase()
     }
 
     for (let i = 0, j = 0, k = 0; i < name.length; i++) {
       // UperCase names part
       if (name[i] === name[i].toUpperCase()) {
-        if (upperNames === 1) retourPart2 += ' ' + name[i] + ' !'
-        else {
+        if (upperNames === 1) {
+          retourPart3 += ', ' + name[i] + '!'
+          retourPart2 += ' ' + name[i] + ' !'
+        } else {
           j++
-          if (j === 1) retourPart2 += ' ' + name[i]
-          else if (j === upperNames && language === 'fr') {
+          if (j === 1) {
+            retourPart2 += ' ' + name[i]
+            retourPart3 += ', ' + name[i]
+          } else if (j === upperNames && language === 'fr') {
             retourPart2 += ' ET ' + name[i] + ' !'
+            retourPart3 += ' ET ' + name[i] + ' !'
           } else if (j === upperNames && language === 'nl') {
             retourPart2 += ' EN ' + name[i] + ' !'
+            retourPart3 += ' EN ' + name[i] + ' !'
           } else if (j === upperNames) {
             retourPart2 += ' AND ' + name[i] + ' !'
-          } else retourPart2 += ', ' + name[i]
+            retourPart3 += ' AND ' + name[i] + ' !'
+          } else {
+            retourPart2 += ', ' + name[i]
+            retourPart3 += ', ' + name[i]
+          }
         }
       } else {
         // Other case names part
@@ -58,19 +73,19 @@ function greet (name, language = 'en') {
       }
     }
 
+    if (upperNames > 0 && lowerNames === 0) return retourPart3
     if (upperNames === 0) return retourPart1
-    if (lowerNames === 0) return retourPart2
     return retourPart1 + retourPart2
   }
-  if (!name || name === ' ') {
+  if (!name || name.trim().length === 0) {
     if (language === 'fr') return 'Bonjour, mon ami.'
     else if (language === 'nl') return 'Hallo, mijn vriend.'
     else return 'Hello, my friend.'
   }
   if (name === name.toUpperCase()) {
-    if (language === 'fr') return 'BONJOUR, ' + name + '.'
-    else if (language === 'nl') return 'HALLO, ' + name + '.'
-    else return 'HELLO, ' + name + '.'
+    if (language === 'fr') return 'BONJOUR, ' + name + '!'
+    else if (language === 'nl') return 'HALLO, ' + name + '!'
+    else return 'HELLO, ' + name + '!'
   }
   if (language === 'fr') return 'Bonjour, ' + name + '.'
   else if (language === 'nl') return 'Hallo, ' + name + '.'
--- src/app.test.js
+++ src/app.test.js
@@ -12,7 +12,7 @@ test('greeting null', () => {
 })
 
 test('greeting CAPSLOCK name', () => {
-  expect(greet('KENOBI')).toBe('HELLO, KENOBI.')
+  expect(greet('KENOBI')).toBe('HELLO, KENOBI!')
 })
 
 test('greeting 2 names', () => {
@@ -26,21 +26,43 @@ test('greeting more than 2 names', () => {
 })
 
 test('greeting few names in uppercase or not', () => {
-  expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe(
-    'Hello, Amy and Charlotte. AND HELLO BRIAN !'
-  )
+  expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN !')
+  expect(greet(['Amy', 'BRIAN', 'CHARLOTTE'])).toBe('Hello, Amy. AND HELLO BRIAN AND CHARLOTTE !')
+  expect(greet(['AMY', 'BRIAN', 'CHARLOTTE'])).toBe('HELLO, AMY, BRIAN AND CHARLOTTE !')
 })
 
 test('greeting in different languages (fr)', () => {
-  expect(greet('Marie', 'fr')).toBe('Bonjour, Marie.')
+  expect(greet(['Marie'], 'fr')).toBe('Bonjour, Marie.')
+  expect(greet(['MARIE'], 'fr')).toBe('BONJOUR, MARIE!')
+  expect(greet(['Marie', 'Amy'], 'fr')).toBe('Bonjour, Marie et Amy.')
+  expect(greet(['Marie', 'AMY'], 'fr')).toBe('Bonjour, Marie. ET BONJOUR AMY !')
+  expect(greet(['MARIE', 'AMY'], 'fr')).toBe('BONJOUR, MARIE ET AMY !')
+  expect(greet(['Marie', 'Brian', 'AMY'], 'fr')).toBe('Bonjour, Marie et Brian. ET BONJOUR AMY !')
+  expect(greet(['Marie', 'AMY', 'Brian'], 'fr')).toBe('Bonjour, Marie et Brian. ET BONJOUR AMY !')
+  expect(greet(['Marie', 'BRIAN', 'AMY'], 'fr')).toBe('Bonjour, Marie. ET BONJOUR BRIAN ET AMY !')
+  expect(greet(['MARIE', 'BRIAN', 'AMY'], 'fr')).toBe('BONJOUR, MARIE, BRIAN ET AMY !')
 })
 
 test('greeting in different languages (en)', () => {
+  expect(greet(['Marie'], 'en')).toBe('Hello, Marie.')
+  expect(greet(['MARIE'], 'en')).toBe('HELLO, MARIE!')
   expect(greet(['Marie', 'Amy'], 'en')).toBe('Hello, Marie and Amy.')
+  expect(greet(['Marie', 'AMY'], 'en')).toBe('Hello, Marie. AND HELLO AMY !')
+  expect(greet(['MARIE', 'AMY'], 'en')).toBe('HELLO, MARIE AND AMY !')
+  expect(greet(['Marie', 'Brian', 'AMY'], 'en')).toBe('Hello, Marie and Brian. AND HELLO AMY !')
+  expect(greet(['Marie', 'AMY', 'Brian'], 'en')).toBe('Hello, Marie and Brian. AND HELLO AMY !')
+  expect(greet(['Marie', 'BRIAN', 'AMY'], 'en')).toBe('Hello, Marie. AND HELLO BRIAN AND AMY !')
+  expect(greet(['MARIE', 'BRIAN', 'AMY'], 'en')).toBe('HELLO, MARIE, BRIAN AND AMY !')
 })
 
 test('greeting in different languages (nl)', () => {
-  expect(greet(['Marie', 'BRIAN', 'Amy'], 'nl')).toBe(
-    'Hallo, Marie en Amy. EN HALLO BRIAN !'
-  )
+  expect(greet(['Marie'], 'nl')).toBe('Hallo, Marie.')
+  expect(greet(['MARIE'], 'nl')).toBe('HALLO, MARIE!')
+  expect(greet(['Marie', 'Amy'], 'nl')).toBe('Hallo, Marie en Amy.')
+  expect(greet(['Marie', 'AMY'], 'nl')).toBe('Hallo, Marie. EN HALLO AMY !')
+  expect(greet(['MARIE', 'AMY'], 'nl')).toBe('HALLO, MARIE EN AMY !')
+  expect(greet(['Marie', 'Brian', 'AMY'], 'nl')).toBe('Hallo, Marie en Brian. EN HALLO AMY !')
+  expect(greet(['Marie', 'AMY', 'Brian'], 'nl')).toBe('Hallo, Marie en Brian. EN HALLO AMY !')
+  expect(greet(['Marie', 'BRIAN', 'AMY'], 'nl')).toBe('Hallo, Marie. EN HALLO BRIAN EN AMY !')
+  expect(greet(['MARIE', 'BRIAN', 'AMY'], 'nl')).toBe('HALLO, MARIE, BRIAN EN AMY !')
 })
