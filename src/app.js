addb5ed8eb31822fe7d7e83e3a763bc1982d27de
MomzMc <mohamed.mcheik@student.vinci.be>
2023-12-12 16:58:37 +0100

    Add changes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-function greet(name) {
+function greet (name) {
   if (Array.isArray(name)) {
-    let retour = 'Hello';
+    let retour = 'Hello'
     for (let i = 0; i < name.length - 1; i++) {
-      retour += ', ' + name[i];
+      retour += ', ' + name[i]
     }
-    retour += ' and ' + name[name.length - 1] + '.';
-    return retour;
+    retour += ' and ' + name[name.length - 1] + '.'
+    return retour
   }
-  if (!name || name === ' ') return 'Hello, my friend.';
-  if (name === name.toUpperCase()) return 'HELLO, ' + name + '.';
-  return 'Hello, ' + name + '.';
-};
-
+  if (!name || name === ' ') return 'Hello, my friend.'
+  if (name === name.toUpperCase()) return 'HELLO, ' + name + '.'
+  return 'Hello, ' + name + '.'
+}
 
 module.exports = greet
--- src/app.test.js
+++ src/app.test.js
@@ -3,22 +3,24 @@ const greet = require('./app')
 
 test('greeting a user', () => {
   expect(greet('Jean-Kevin')).toBe('Hello, Jean-Kevin.')
-});
+})
 
 test('greeting null', () => {
   expect(greet()).toBe('Hello, my friend.')
   expect(greet(undefined)).toBe('Hello, my friend.')
   expect(greet(' ')).toBe('Hello, my friend.')
-});
+})
 
 test('greeting CAPSLOCK name', () => {
   expect(greet('KENOBI')).toBe('HELLO, KENOBI.')
-});
+})
 
 test('greeting 2 names', () => {
-  expect(greet(['Jill','Jane'])).toBe('Hello, Jill and Jane.')
-});
+  expect(greet(['Jill', 'Jane'])).toBe('Hello, Jill and Jane.')
+})
 
 test('greeting more than 2 names', () => {
-  expect(greet(['Amy','Brian','Charlotte'])).toBe('Hello, Amy, Brian and Charlotte.')
-});
+  expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe(
+    'Hello, Amy, Brian and Charlotte.'
+  )
+})
