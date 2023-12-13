8da990a5020c753a277e1c5d15512c4d6a5f2793
MomzMc <mohamed.mcheik@student.vinci.be>
2023-12-13 21:22:35 +0100

    Update: Eslint and Prettier confing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {}
-}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'prettier',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    rules: {
+        semi: [1, 'always'],
+    },
+};
--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,6 @@
-/** @type {import("prettier").Config} */
-const config = {
-  trailingComma: 'es5',
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true
-}
-
-module.exports = config
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+};
